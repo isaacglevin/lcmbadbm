3fd578e081449bb14ec9cdd83c0778e22cb5c729
isaacglevin <isaacglevin@gmail.com>
2025-03-11 16:19:42 -0400

    Updated JavaDocs in App.java, DiskMark.java, and package-info.java

--- src/edu/touro/mco152/bm/persist/package-info.java
+++ src/edu/touro/mco152/bm/persist/package-info.java
@@ -1,6 +1,18 @@
 /**
- * A container for utility classes that help with handling persistent
- * data. This data can be used to gather statistical information
- * about the machine being tested.
+ * Provides core classes for the BadBM benchmarking tool.
+ * <p>
+ * This package includes the main application logic, disk benchmarking utilities,
+ * GUI management, and data persistence components. The primary classes in this package
+ * are:
+ * </p>
+ * <ul>
+ *     <li>{@link edu.touro.mco152.bm.App} - Main entry point and configuration manager.</li>
+ *     <li>{@link edu.touro.mco152.bm.DiskMark} - Represents a single disk benchmark measurement.</li>
+ *     <li>{@link edu.touro.mco152.bm.DiskWorker} - Handles benchmark execution in a separate thread.</li>
+ * </ul>
+ *
+ * @author Isaac Levin
+ * @version 1.0
  */
+
 package edu.touro.mco152.bm.persist;
